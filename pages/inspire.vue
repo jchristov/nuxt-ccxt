bb48ee701113747cb5f5f085753a8ec8990ca400
hijiri001 <shy@oboe.club.ne.jp>
2018-05-16 09:33:37 +0900

    .gitignore is now working

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,6 +2,13 @@
   <v-layout>
     <v-flex text-xs-center>
       <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <ul class="users">
+        <li v-for="(user, index) in users" :key="index" class="user">
+          <nuxt-link :to="{ name: 'id', params: { id: index }}">
+            {{ user.name }}
+          </nuxt-link>
+        </li>
+      </ul>
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
@@ -13,3 +20,35 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import axios from '~/plugins/axios'
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: 'Users'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin: 30px 0;
+}
+.users
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.user
+{
+  margin: 10px 0;
+}
+</style>
--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 
 import users from './users'
+import ccxt from './ccxt'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
+router.use(ccxt)
 
 export default router
